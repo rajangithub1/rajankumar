2814f793d02bf2802e3cecd370db17a57b3191ff
rajangithub1 <161829965+rajangithub1@users.noreply.github.com>
2024-03-01 06:08:51 -0800

    Add files via upload

---
+++ javascript/first.js
@@ -0,0 +1,11 @@
+
+// let a = 5;
+// let b = 5;
+// console.log("5>4", a<=b)
+// let age = 11;
+// if(age>18){
+//     console.log('can vote');
+// }
+// if(age<18){console.log('you cant vote');
+// }
+console.log('rajan')
